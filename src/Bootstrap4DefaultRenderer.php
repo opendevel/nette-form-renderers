242f09ec2f1c12272ffb18a065c69e39e20050d6
Robert Sipek <rob.sipek@gmail.com>
2019-12-21 20:36:36 +0100

    Create empty class Bootstrap4DefaultFormRenderer

---
+++ src/Bootstrap4DefaultRenderer.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace Opendevel\Nette\Forms\Rendering;
+
+use Nette\Forms\Form;
+
+class Bootstrap4DefaultFormRenderer implements \Nette\Forms\IFormRenderer
+{
+    use \Nette\SmartObject;
+
+
+    /**
+     * @inheritDoc
+     */
+    function render(Form $form): string
+    {
+        // TODO: Implement render() method.
+    }
+}
