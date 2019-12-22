a0d9fb1e5b03822c78b2d73f8983d5efaf68b20d
Robert Sipek <rob.sipek@gmail.com>
2019-12-22 20:50:33 +0100

    Temporarily limit render subcontainer only inside fieldset

--- src/Bootstrap4DefaultFormRenderer.php
+++ src/Bootstrap4DefaultFormRenderer.php
@@ -262,10 +262,12 @@ class Bootstrap4DefaultFormRenderer implements IFormRenderer
                 $container->id = $group->getOption('id', 'group-' . $groupKey);
             }
 
+            if ($container->getName() === 'fieldset') { //@todo odstranit/zmenit
             $subcontainer = $group->getOption('subcontainer', $this->getWrapper('group subcontainer'));
             $subcontainer = $subcontainer instanceof Html ? clone $subcontainer : Html::el($subcontainer);
 
             $container->addHtml($subcontainer);
+            }
 
             $s .= "\n" . $container->startTag();
 
